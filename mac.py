f4b078eb1782688b9a9ec7bb5daebe371571e953
Abhishek1434 <abhishek.chitturi@gmail.com>
2023-05-15 16:47:31 +0530

    changed https to http

--- mac.py
+++ mac.py
@@ -9,7 +9,7 @@ def getVendorName(x):
 
     if re.match("[0-9a-f]{2}([-:.])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", x.lower()):
         vendor = requests.get(
-            'https://api.macaddress.io/v1?apiKey=at_vlI1yrNeMikipomW3wPWBoHzDZkOi&output=vendor&search='
+            'http://api.macaddress.io/v1?apiKey=at_vlI1yrNeMikipomW3wPWBoHzDZkOi&output=vendor&search='
             + x).text
         print "Vendor of ""\"%s\"" % x, "is :", vendor
 
